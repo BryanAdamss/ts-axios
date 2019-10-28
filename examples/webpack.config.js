db324a81356fbe87d1aa9e152729caa1a0796353
BryanAdamss <BryanAdamss@foxmail.com>
2019-10-28 22:29:30 +0800

    chore(pkg): 更新依赖

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -16,6 +16,7 @@ module.exports = {
   entry: fs.readdirSync(__dirname).reduce((entries, dir) => {
     const fullDir = path.join(__dirname, dir)
     const entry = path.join(fullDir, 'app.ts')
+
     if (fs.statSync(fullDir).isDirectory() && fs.existsSync(entry)) {
       entries[dir] = ['webpack-hot-middleware/client', entry]
     }
